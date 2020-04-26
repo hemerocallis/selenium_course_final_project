f734a33f040f335601bfa5e4e50d2c21484ff4bd
hemera <hemerocallisongml@gmail.com>
2020-04-26 15:19:37 +0700

    is_element_present() method is implemented

--- test_main_page.py
+++ test_main_page.py
@@ -5,5 +5,10 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link) # Page Object initialization
     page.open()
+    page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link) # Page Object initialization
+    page.open()
     page.should_be_login_link()
-    # page.go_to_login_page()
