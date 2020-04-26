8155cfba370aab334e84fcbdbd6a01cc9fe90988
hemera <hemerocallisongml@gmail.com>
2020-04-26 19:42:47 +0700

    test case is fixed: lost import is added

--- test_main_page.py
+++ test_main_page.py
@@ -8,7 +8,7 @@ def test_guest_can_go_to_login_page(browser):
     main_page.open()
     main_page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_url()
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
