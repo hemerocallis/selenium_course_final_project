884db06cb33c2582feceb582c7fbc8661d7ce736
hemera <hemerocallisongml@gmail.com>
2020-04-26 16:16:59 +0700

    LoginPage class is added

---
+++ pages/login_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        login_url_substring = "login"
+        login_url = self.browser.current_url
+        assert login_url_substring in login_url, "Incorrect login URL"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not found"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Registration form is not found"
