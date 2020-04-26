d43f3ab23a41517d5f03545f04031cd2d1e5ca63
hemera <hemerocallisongml@gmail.com>
2020-04-27 06:55:48 +0700

    ProductPage methods are added

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,20 +6,24 @@ class ProductPage(BasePage):
         add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_button.click()
 
-    def should_be_correct_product_name(self):
-        alert_success = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_BASKET_ALERT)
-        alert_text = alert_success.text
+    def get_product_name(self):
         product_name_label = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_LABEL)
         product_name = product_name_label.text
+        return product_name
 
+    def get_product_price(self):
+        product_price_label = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_LABEL)
+        product_price = product_price_label.text
+        return product_price
+
+    def should_be_correct_product_name(self, product_name):
+        alert_success = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_BASKET_ALERT)
+        alert_text = alert_success.text
         assert product_name in alert_text, "Incorrect product name"
 
-    def should_be_correct_basket_price(self):
-        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_LABEL)
-        product_price_text = product_price.text
+    def should_be_correct_basket_price(self, product_price):
         basket_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE_ALERT)
         basket_price_text = basket_price.text
-
-        assert product_price_text == basket_price_text, "Incorrect basket price"
+        assert product_price == basket_price_text, "Incorrect basket price"
 
 
--- test_product_page.py
+++ test_product_page.py
@@ -2,12 +2,15 @@ from .pages.product_page import ProductPage
 import time
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product_to_basket()
     product_page.solve_quiz_and_get_code()
-    product_page.should_be_correct_product_name()
-    product_page.should_be_correct_basket_price()
-    time.sleep(7)
 
+    product_name = product_page.get_product_name()
+    product_page.should_be_correct_product_name(product_name)
+
+    product_price = product_page.get_product_price()
+    product_page.should_be_correct_basket_price(product_price)
