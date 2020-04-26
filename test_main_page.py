2690c6988e3c5fb8685cfbcd36b4ca48af2cfd43
hemera <hemerocallisongml@gmail.com>
2020-04-26 14:34:40 +0700

    my first Page Object Model based test

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,8 @@
-BASE_URL = "http://selenium1py.pythonanywhere.com/"
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
 
 # pytest -v --tb=line --language=en test_main_page.py
 def test_guest_can_go_to_login_page(browser):
-    browser.get(BASE_URL)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link) # Page Object initialization
+    page.open()
+    page.go_to_login_page()
